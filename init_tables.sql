ab65a4e20db0aad59d2cdb4358345007529f19c6
zealorez <zealorez@gmail.com>
2021-03-08 11:31:14 +0800

    modified init_tables.sql

--- init_tables.sql
+++ init_tables.sql
@@ -5,20 +5,20 @@ CREATE TABLE employees (
 	password TEXT,
 	job_title_id INT,
 	manager_id INT
-)
+);
 
 CREATE TABLE general_competencies (
 	id SERIAL PRIMARY KEY,
 	competency TEXT,
   description TEXT
-)
+);
 
 CREATE TABLE general_levels (
 	id SERIAL PRIMARY KEY,
 	level TEXT,
 	description TEXT,
 	general_competency_id INT
-)
+);
 
 CREATE TABLE job_titles (
 	id SERIAL PRIMARY KEY,
@@ -29,20 +29,20 @@ CREATE TABLE job_titles (
 CREATE TABLE job_categories (
 	id SERIAL PRIMARY KEY,
 	name TEXT
-)
+);
 
 CREATE TABLE general_job_requirement (
 	id SERIAL PRIMARY KEY,
 	job_title_id INT,
 	general_levels_id INT
-)
+);
 
 CREATE TABLE evaluations (
 	id SERIAL PRIMARY KEY,
 	employee_id INT,
 	date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	status TEXT
-)
+);
 
 CREATE TABLE employee_competencies (
 	id SERIAL PRIMARY KEY,
@@ -52,4 +52,4 @@ CREATE TABLE employee_competencies (
 	manager_level_id INT,
 	manager_comment TEXT,
 	evaluations_id INT
-)
+);
