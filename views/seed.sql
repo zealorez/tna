c5bae5177b35759d62b4f6863b490a90c853eb64
zealorez <zealorez@gmail.com>
2021-03-08 11:14:18 +0800

    updated seed data

--- views/seed.sql
+++ views/seed.sql
@@ -1,15 +1,20 @@
-COPY general_competencies(competency)
-FROM '/home/zephaniah/Desktop/tna/CompetencyDatabase.csv'
+COPY general_competencies(competency, description)
+FROM 'general_competencies.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY job_titles(job_title)
-FROM '/home/zephaniah/Desktop/database/hrJobRoles.csv'
+COPY job_titles(job_title, job_category_id)
+FROM 'job_titles_HR.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY general_job_requirement(job_title_id, general_levels_id)
-FROM '/home/zephaniah/Desktop/database/generalJobRequirementHr.csv'
+COPY general_job_requirements(job_title_id, general_levels_id)
+FROM 'general_job_requirements_HR.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY general_levels(general_competency_id, level, description)
+FROM 'general_levels.csv'
 DELIMITER ','
 CSV HEADER;
 
@@ -17,6 +22,3 @@ INSERT INTO job_category (name) VALUES ('HR');
 INSERT INTO job_category (name) VALUES ('ICT');
 INSERT INTO job_category (name) VALUES ('Logistics');
 INSERT INTO job_category (name) VALUES ('Training and Education');
-
-job_titles => job_titles_HR.csv
-general_job_requirements => general_job_requirements_HR.csv
