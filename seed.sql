d89f13f58627c61db9042b25aad1925396b1bf1b
zealorez <zealorez@gmail.com>
2021-03-08 11:35:07 +0800

    modified seed.sql

--- seed.sql
+++ seed.sql
@@ -1,24 +1,24 @@
 COPY general_competencies(competency, description)
-FROM 'general_competencies.csv'
+FROM '/home/ubuntu/tna/general_competencies.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY job_titles(job_title, job_category_id)
-FROM 'job_titles_HR.csv'
+FROM '/home/ubuntu/tna/job_titles_HR.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY general_job_requirements(job_title_id, general_levels_id)
-FROM 'general_job_requirements_HR.csv'
+FROM '/home/ubuntu/tna/general_job_requirements_HR.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY general_levels(general_competency_id, level, description)
-FROM 'general_levels.csv'
+FROM '/home/ubuntu/tna/general_levels.csv'
 DELIMITER ','
 CSV HEADER;
 
-INSERT INTO job_category (name) VALUES ('HR');
-INSERT INTO job_category (name) VALUES ('ICT');
-INSERT INTO job_category (name) VALUES ('Logistics');
-INSERT INTO job_category (name) VALUES ('Training and Education');
+INSERT INTO job_categories (name) VALUES ('HR');
+INSERT INTO job_categories (name) VALUES ('ICT');
+INSERT INTO job_categories (name) VALUES ('Logistics');
+INSERT INTO job_categories (name) VALUES ('Training and Education');
