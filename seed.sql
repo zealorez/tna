b9250585b32f74cb5cc2256528f52e56ea3dfb22
zealorez <zealorez@gmail.com>
2021-03-10 19:55:27 +0800

    modified copy command to \copy

--- seed.sql
+++ seed.sql
@@ -1,19 +1,19 @@
-COPY general_competencies(competency, description)
+\copy general_competencies(competency, description)
 FROM '/home/ubuntu/tna/general_competencies.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY job_titles(job_title, job_category_id)
+\copy job_titles(job_title, job_category_id)
 FROM '/home/ubuntu/tna/job_titles_HR.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY general_job_requirements(job_title_id, general_levels_id)
+\copy general_job_requirement(job_title_id, general_levels_id)
 FROM '/home/ubuntu/tna/general_job_requirements_HR.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY general_levels(general_competency_id, level, description)
+/copy general_levels(general_competency_id, level, description)
 FROM '/home/ubuntu/tna/general_levels.csv'
 DELIMITER ','
 CSV HEADER;
