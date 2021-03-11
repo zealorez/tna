66e9d471ae222235e5e5e22fa50875d21a673f0b
zealorez <zealorez@gmail.com>
2021-03-11 08:03:38 +0800

    modified seed.sql

--- seed.sql
+++ seed.sql
@@ -1,22 +1,10 @@
-\copy general_competencies(competency, description)
-FROM '/home/ubuntu/tna/general_competencies.csv'
-DELIMITER ','
-CSV HEADER;
+\copy general_competencies(competency, description) FROM '/home/zephaniah/tna/general_competencies.csv' DELIMITER ',' CSV HEADER;
 
-\copy job_titles(job_title, job_category_id)
-FROM '/home/ubuntu/tna/job_titles_HR.csv'
-DELIMITER ','
-CSV HEADER;
+\copy job_titles(job_title, job_category_id) FROM '/home/zephaniah/tna/job_titles_HR.csv' DELIMITER ',' CSV HEADER;
 
-\copy general_job_requirement(job_title_id, general_levels_id)
-FROM '/home/ubuntu/tna/general_job_requirements_HR.csv'
-DELIMITER ','
-CSV HEADER;
+\copy general_job_requirement(job_title_id, general_levels_id) FROM '/home/zephaniah/tna general_job_requirements_HR.csv' DELIMITER ',' CSV HEADER;
 
-/copy general_levels(general_competency_id, level, description)
-FROM '/home/ubuntu/tna/general_levels.csv'
-DELIMITER ','
-CSV HEADER;
+\copy general_levels(general_competency_id, level, description) FROM '/home/zephaniah/tna/general_levels.csv' DELIMITER ',' CSV HEADER;
 
 INSERT INTO job_categories (name) VALUES ('HR');
 INSERT INTO job_categories (name) VALUES ('ICT');
